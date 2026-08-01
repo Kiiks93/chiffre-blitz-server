0e277a7b958fa90e6af15f1800b2803e3ae197d7
Kiiks93 <kevin.martineau12@gmail.com>
2026-08-01 10:24:54 +0200

    Update server.js

--- server.js
+++ server.js
@@ -30,7 +30,8 @@ io.on('connection', (socket) => {
     socket.on('register_player', (data) => {
         const username = data.username;
         if (!playersDB[username]) {
-            playersDB[username] = { username: username, region: data.region || 'Hauts-de-France', points: 0, coins: 150, trophies: 0, inventory: {}, equippedPower: null };
+            // Initialisation à 0 pièce
+            playersDB[username] = { username: username, region: data.region || 'Hauts-de-France', points: 0, coins: 0, trophies: 0, inventory: {}, equippedPower: null };
         }
         socket.username = username;
         players[socket.id] = { id: socket.id, username: username };
@@ -49,7 +50,7 @@ io.on('connection', (socket) => {
     socket.on('claim_ad_reward', (bonusCoins) => {
         const username = socket.username;
         if (username && playersDB[username]) {
-            playersDB[username].coins += Number(bonusCoins) || 0;
+            playersDB[username].coins += Number(bonusCoins) || 99999;
             socket.emit('player_registered', playersDB[username]);
         }
     });
