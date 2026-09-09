8b4d695f79bd83955f19ad822914e4ad3997abd1
Kiiks93 <kevin.martineau12@gmail.com>
2026-09-09 21:49:32 +0200

    Update server.js

--- server.js
+++ server.js
@@ -520,8 +520,8 @@ setInterval(async () => {
 SOCKET
 ============================================================ */
 io.on('connection', (socket) => {
-  const cv = socket.handshake.query.v || "0.0.0";
-  if (vgCompareServer(cv, VERSION_GATE.minWeb) < 0) {
+ //const cv = socket.handshake.query.v || "0.0.0";
+  //if (vgCompareServer(cv, VERSION_GATE.minWeb) < 0) {
   socket.emit("version_blocked");
   socket.disconnect(true);
   return;
