a94501f77015a22e513c2cb4d80935626ab133d0
Kiiks93 <kevin.martineau12@gmail.com>
2026-09-11 12:23:30 +0200

    Update server.js

--- server.js
+++ server.js
@@ -325,9 +325,16 @@ setInterval(() => {
 }, 5000);
 
 app.get('/', (req, res) => { res.send('Chiffre Blitz Server is running ⚡'); });
+
+// AJOUTE CES LIGNES :
+const path = require('path');
+
 app.get('/admin.html', (req, res) => { 
-  res.sendFile(__dirname + '/admin.html'); 
+  res.sendFile(path.join(__dirname, 'admin.html')); 
 });
+
+// Optionnel : servir aussi d'autres fichiers statiques si besoin
+app.use(express.static(path.join(__dirname, '.')));
 
 async function savePlayerToSupabase(socketId) {
   const p = activePlayers[socketId];
