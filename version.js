c6d311d13255640b2ab03b2c2e1b66b098c8cf15
Kiiks93 <kevin.martineau12@gmail.com>
2026-09-24 22:49:13 +0200

    Update version.js

--- version.js
+++ version.js
@@ -8,7 +8,7 @@ WORKFLOW À CHAQUE RELEASE :
 ============================================================ */
 const VERSION_CLIENT = {
   version: "1.4.0",                                  // version WEB actuelle (semver)
-  shell: 10,                                          // versionCode Capacitor (Android)
+  shell: 11,                                          // versionCode Capacitor (Android)
   serverUrl: window.location.hostname.endsWith('.onrender.com') ? window.location.origin : 'https://chiffre-blitz-server.onrender.com'  // ← ton URL Render
 };
 
